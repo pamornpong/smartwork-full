ebfbc6ed5b251094347c9b031a3ada1b5cebbc6d
pamornpong <68239197+pamornpong@users.noreply.github.com>
2025-11-20 12:40:41 +0700

    Create confijg.js
    new config js

---
+++ confijg.js
@@ -0,0 +1,3 @@
+const SCRIPT_URL =
+  "https://script.google.com/macros/s/AKfycbydlfrI9yERCaTPY-qxlvccD_DdKpZmlKWrAgsOl6iGCqbZfBla-E32bNTB2PfObe8-mw/exec";
+const API_URL = SCRIPT_URL + "?v=" + Date.now();
